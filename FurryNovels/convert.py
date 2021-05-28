f76688fc7de406ee1629f5cc88c9ae383a8c1c35
Downey Rem <73387736+DowneyRem@users.noreply.github.com>
2021-05-28 20:26:28 +0800

    Add files via upload

--- FurryNovels/convert.py
+++ FurryNovels/convert.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import os
+import time
 from opencc import OpenCC
-
-
+from functools import wraps
 cc1 = OpenCC('tw2s') #繁转简
 cc2 = OpenCC('s2tw') #簡轉繁
-path = "D:\\兽人小说\\小说推荐"
-path1 = "D:\\兽人小说\\小说转换版\\简体版"
-path2 = "D:\\兽人小说\\小说转换版\\繁體版"
 
-   
+
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        r = func(*args, **kwargs)
+        end = time.perf_counter()
+        print('{}.{} : {}'.format(func.__module__, func.__name__, end - start))
+        return r
+    return wrapper
+
+
 def findtxt(path):
     for dir in os.listdir(path):
         dir = os.path.join(path, dir)
@@ -35,33 +43,42 @@ def savefile(path, text):
             f.close()
         
         
+@timethis
 def convert(list):
-    for i in range(0,len(list)):
+    for i in range(0, len(list)):
         readfile =list[i]
-        
         (filepath, name) = os.path.split(readfile) #分离文件名和目录名
-        filepath = filepath.replace(path, "")      #留下文件夹名，新建目录
-        makedirs(path1 + filepath)
-        makedirs(path2 + filepath)
-
-        with open(readfile,"r", encoding = "UTF-8") as f:
-            (tempath, name) = os.path.split(f.name)  #带拓展名的文件名
-            text = f.read()
+        filepath = filepath.replace(path, "")      #留下文件夹名，#新建目录
+        
+        name1 = cc1.convert(name)  #转简体
+        name2 = cc2.convert(name)  #转繁体
+        path11 = os.path.join(path1 + filepath, name1)  #简体目录
+        path22 = os.path.join(path2 + filepath, name2)  #繁体目录
+        
+        if os.path.exists(path11) and os.path.exists(path22): 
+            i += 1
+        else:
+            makedirs(path1 + filepath)
+            makedirs(path2 + filepath)
             
-            if "會"or"後"or"來"or"東"or"電"or"個" in text: #文件是繁体
-                name = cc1.convert(name) #繁体转简体，存简体目录
-                text = cc1.convert(text)
-                savefile(os.path.join(path1 + filepath, name), text)
+            with open(readfile,"r", encoding = "UTF-8") as f:
+                text = f.read()
+
+                # "會"or"後"or"來"or"東"or"電"or"個" in text: #文件是繁体
+                text = cc1.convert(text) #繁体转简体，存简体目录
+                savefile(os.path.join(path11), text)
                 
-            if "会"or"来"or"东"or"电"or"个" in text: #文件是简体
-                name = cc2.convert(name) #簡體轉繁體，存繁體目錄
-                text = cc2.convert(text)
-                savefile(os.path.join(path2 + filepath, name), text)
                 
-            f.close
+                # "会"or"来"or"东"or"电"or"个" in text: #文件是简体
+                text = cc2.convert(text) #簡體轉繁體，存繁體目錄
+                savefile(os.path.join(path22), text)
+                print("【"+name + "】转换成功")
+                
+                f.close
         print("【"+ cc1.convert(name) + "】已完成转换，当前进度："+ str(100*(i+1)/len(list))+"%")
         
         
+@timethis
 def main(): 
     print("繁简转换开始：")
     print("————————————————")
@@ -78,5 +95,7 @@ def main():
 
 
 if __name__ == '__main__':
-    list = []
-    main()
+    path = os.path.join(os.getcwd() + "\小说推荐")
+    path1 = os.path.join(os.getcwd() + "\小说转换版\简体版")
+    path2 = os.path.join(os.getcwd() + "\小说转换版\繁體版")
+    list = []; main()
