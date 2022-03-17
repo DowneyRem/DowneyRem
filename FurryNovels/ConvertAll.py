47ea8d6a06b19863e241f9b1b951d9fdd3eed9e6
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2022-03-17 15:52:54 +0800

    update

--- FurryNovels/ConvertAll.py
+++ FurryNovels/ConvertAll.py
@@ -21,18 +21,18 @@ def timethis(func):
 	return wrapper
 
 
-def finddocx(path):
+def findDocx(path):
 	for dir in os.listdir(path):
 		dir = os.path.join(path, dir)
 		if os.path.isdir(dir):
-			finddocx(dir)
+			findDocx(dir)
 		if os.path.isfile(dir):
 			(name, ext) = os.path.splitext(dir)
 			if ext == ".docx":
 				list.append(dir)
 	return list
 
-def opendocx(path):
+def openDocx(path):
 	(dir, name) = os.path.split(path)  # 分离文件名和目录名
 	(name, ext) = os.path.splitext(name)
 	
@@ -45,10 +45,10 @@ def opendocx(path):
 			else:
 				text += para.text + "\n"  # 除正文缩进外的其他所有
 	except:
-		print("【" + name + "】保存失败")
+		print("【" + name + "】打开失败")
 	return text
 
-def opentext(path):
+def openText(path):
 	text = ""
 	try:
 		with open(path,"r", encoding = "UTF8") as f:
@@ -64,29 +64,28 @@ def opentext(path):
 		return text
 	
 	
-def makedirs(path):
+def makeDirs(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 
-def savetext(path, text):
+def saveText(path, text):
 	(dir, name) = os.path.split(path)  # 分离文件名和目录名
 	name = name.replace(".txt", "")
-	
-	if not os.path.exists(dir):
-		os.makedirs(dir)
+	makeDirs(dir)
 	try:
 		with open(path, "w", encoding="UTF8") as f:
 			f.write(text)
 	except IOError:
 		print("【" + name + "】保存失败")
 
-def removefile(path):
+
+def removeFile(path):
 	if os.path.exists(path):
 		shutil.rmtree(path)
 	os.makedirs(path)
 	
 	
-def getpath(path):
+def getPath(path):
 	path0 = path.replace("\小说推荐", "\兽人小说\小说推荐\频道版")
 	path0 = path0.replace(".docx", ".txt")
 	
@@ -99,26 +98,39 @@ def getpath(path):
 	return path0, path1, path2, name
 
 
+def translateText(text, path1, path2):
+	list1 = ["邊", "變", "並", "從", "點", "東", "對", "發", "該", "個", "給", "關", "過", "還", "後", "歡", "會", "機", "幾", "間", "見", "將", "進", "經", "覺", "開", "來", "裡", "兩", "嗎", "麼", "沒", "們", "難", "讓", "時", "實", "說", "雖", "為", "問", "無", "現", "樣", "應", "於", "與", "則", "這", "種"]
+	list2 = ["边", "变", "并", "从", "点", "东", "对", "发", "该", "个", "给", "关", "过", "还", "后", "欢", "会", "机", "几", "间", "见", "将", "进", "经", "觉", "开", "来", "里", "两", "吗", "么", "没", "们", "难", "让", "时", "实", "说", "虽", "为", "问", "无", "现", "样", "应", "于", "与", "则", "这", "种"]
+	# 语料库来自 https://elearning.ling.sinica.edu.tw/cwordfreq.html
+	# 从中选取前三百的繁体字部分，并在文章中随机检验，取存在率最高的前50个繁体字符
+	
+	j = 0
+	for i in range(len(list1)):
+		char = list1[i]
+		if char in text:
+			j += 1
+	
+	if j >= 0.2 * len(list1):
+		text1 = cc1.convert(text)  # 繁体文件，转简体
+		saveText(path2, text)      # 复制后，存繁体目录
+		saveText(path1, text1)     # 转简体，存简体目录
+	else:
+		text2 = cc2.convert(text)  # 简体文件，转繁体
+		saveText(path1, text)      # 复制后，存简体目录
+		saveText(path2, text2)     # 转繁体，存繁体目录
+
+
 def convert(list):
 	for i in range(0, len(list)):
 		path = list[i]
-		(path0, path1, path2, name) = getpath(path)
-		
+		(path0, path1, path2, name) = getPath(path)
 		if os.path.exists(path0):
 			i += 1
-			print("【" + name + "】在本次运行前已转换")
+			# print("【" + name + "】在本次运行前已转换")
 		else:
-			text = opendocx(path)
-			savetext(path0, text)  # 不区分繁简，存频道目录
-			if "會"in text or "後"in text or "來"in text or "東"in text or "電"in text or "個"in text:
-				text1 = cc1.convert(text)  # 转简体
-				savetext(path2, text)  # 繁体文件复制后，存繁体目录
-				savetext(path1, text1)  # 繁体文件转简体，存简体目录
-	
-			elif "会"in text or "来"in text or "东"in text or "电"in text or "个"in text:
-				text2 = cc2.convert(text)  # 转繁体
-				savetext(path1, text)  # 简体文件复制后，存简体目录
-				savetext(path2, text2)  # 简体文件转繁体，存繁体目录
+			text = openDocx(path)
+			saveText(path0, text)  # 不区分繁简，存频道目录
+			translateText(text, path1, path2)
 			print("【"+ name +"】转换成功，当前进度：" + str(round(100*(i+1)/len(list),2))+"%")
 
 
@@ -126,14 +138,14 @@ def main():
 	print("是否要【删除旧文件】重新转换？")
 	string = input("输入【 1 】即【删除旧文件】"+"\n"*2)
 	if str(1) in string:
-		removefile(sharepath)
+		removeFile(sharepath)
 		print("【删除】旧文件")
 	else:
 		print("【保留】旧文件")
 		
 	print("文档转换开始：")
 	print("-"*40)
-	finddocx(path)
+	findDocx(path)
 	convert(list)
 	print("-" * 40)
 	print("文档转换已完成")
--- FurryNovels/PrintTags.py
+++ FurryNovels/PrintTags.py
@@ -38,8 +38,8 @@ dict = {
 		"original":"Original",
 		"同人":"Doujin",
 		"doujin":"Doujin",
-		"翻译":"Translated",
-		"translated":"Translated",
+		"翻译":"translated",
+		"translated":"translated",
   
 		"sfw":"SFW",
 		"R-18":"R18",
@@ -406,7 +406,7 @@ def timethis(func):
 	return wrapper
 
 
-def monthnow():
+def monthNow():
 	year = str(time.localtime()[0])
 	month = str(time.localtime()[1])
 	if len(month) == 1:
@@ -415,20 +415,23 @@ def monthnow():
 	return string
 
 
-def opennowdir():
+def openNowDir():
 	path = os.getcwd()
 	path = path.replace("\小说推荐\工具", "\兽人小说\小说推荐\频道版")
-	text = monthnow()
+	text = monthNow()
 	path = os.path.join(path,text)
 	os.system('start explorer '+ path)
 
 
 def cmp(a, b):
 	def getindex(a):
-		li = list(dict.values())
-		index = li.index(a)
+		try:
+			li = list(dict.values())
+			index = li.index(a)
+		except:
+			li = list(dict.keys())
+			index = li.index(a)
 		return index
-	
 	a = getindex(a)
 	b = getindex(b)
 	if a > b:
@@ -439,17 +442,7 @@ def cmp(a, b):
 		return 0
 
 
-def sorttags(set):  # 按dict内顺序对转换后的标签排序
-	text = ""
-	li = list(set)
-	li.sort(key=cmp_to_key(cmp))
-	for i in range(len(li)):
-		tag = li[i]
-		text += "#" + tag + " "
-	return text
-
-
-def savetext(path, text):
+def saveText(path, text):
 	(dir, name) = os.path.split(path)  # 分离文件名和目录名
 	name = name.replace(".txt", "")
 	if not os.path.exists(dir):
@@ -461,7 +454,7 @@ def savetext(path, text):
 		print("【" + name + "】保存失败")
 
 
-def savedict2md():
+def saveDict2md():
 	text = "### 关键词标签表\n"
 	text = text + "\n| 标签 | 关键词 | "
 	text = text + "\n| -- | -- | "
@@ -471,26 +464,42 @@ def savedict2md():
 		if value != "":
 			value = "#" + value
 			text += "\n| " + value + " | " + key + " |"
-	
+			
 	path = os.path.join(os.getcwd(), "Tags.md")
-	savetext(path, text)
+	saveText(path, text)
 	
 	
-def savetags2text(set):
+def set2text(set):
 	text = str(set)
 	text = text.replace("{'", "")
 	text = text.replace("'}", "")
 	text = text.replace("', '", " ")
 	text = text.replace(" ", "\n")
-	path = "D:\\Users\\Administrator\\Desktop\\tag.txt"
-	savetext(path, text)
+	return text
 
+
+def saveTextDesktop(name, set):
+	path = "D:\\Users\\Administrator\\Desktop"
+	path = os.path.join(path, name)
+	text = set2text(set)
+	saveText(path, text)
 	
-def findfile(path):
+	
+def sortTags(set):  # 按dict内顺序对转换后的标签排序
+	text = ""
+	li = list(set)
+	li.sort(key=cmp_to_key(cmp))
+	for i in range(len(li)):
+		tag = li[i]
+		text += "#" + tag + " "
+	return text
+	
+	
+def findFile(path):
 	for dir in os.listdir(path):
 		dir = os.path.join(path, dir)
 		if os.path.isdir(dir):
-			findfile(dir)
+			findFile(dir)
 		if os.path.isfile(dir):
 			(name, ext) = os.path.splitext(dir)
 			if ext == ".docx" or ext == ".txt":
@@ -498,7 +507,7 @@ def findfile(path):
 	return pathlist
 
 
-def opentext4(path):
+def openText4(path):
 	textlist =[]
 	try:
 		with open(path, "r", encoding="UTF8") as f:
@@ -512,25 +521,89 @@ def opentext4(path):
 				textlist = f.readlines()[0:4]
 	finally:
 		return textlist
+
 	
-	
-def opendocx4(path):
-	docx = Document(path)
-	text = "";  j = 1
-	for para in docx.paragraphs:
-		if j < 5:  # 只读取前4行内容
-			j += 1
+def openDocx4(path):
+	try:
+		docx = Document(path)
+		text = "";  j = 1
+		for para in docx.paragraphs:
+			if j < 5:  # 只读取前4行内容
+				j += 1
+				if para.style.name == "Normal Indent":  # 正文缩进
+					text += "　　" + para.text + "\n"
+				else:
+					text += para.text + "\n"  # 除正文缩进外的其他所有
+			else:
+				break
+		textlist = text2List(text) 	# 写入txt，以readlines的方式再次读取，获得段落对应的list
+		return textlist
+	except IOError:
+		print("文件打开失败")
+
+
+def openText(path):
+	text = ""
+	try:
+		with open(path,"r", encoding = "UTF8") as f:
+			text = f.read()
+	except UnicodeError:
+		try:
+			with open(path,"r", encoding = "GBK") as f:
+				text = f.read()
+		except UnicodeError: #Big5 似乎有奇怪的bug，不过目前似乎遇不到
+			with open(path,"r", encoding = "BIG5") as f:
+				text = f.read()
+	finally:
+		return text
+
+
+def openDocx(path):
+	try:
+		docx = Document(path)
+		text = ""
+		for para in docx.paragraphs:
 			if para.style.name == "Normal Indent":  # 正文缩进
 				text += "　　" + para.text + "\n"
 			else:
 				text += para.text + "\n"  # 除正文缩进外的其他所有
-		else:
-			break
-	textlist = text2list(text) 	# 写入txt，以readlines的方式再次读取，获得段落对应的list
-	return textlist
+		return text
+	except IOError:
+		print("文件打开失败")
+		
+
+def addTags(text): #添加靠谱的标签
+	list1 = ["邊", "變", "並", "從", "點", "東", "對", "發", "該", "個", "給", "關", "過", "還", "後", "歡", "會", "機", "幾", "間", "見", "將", "進", "經", "覺", "開", "來", "裡", "兩", "嗎", "麼", "沒", "們", "難", "讓", "時", "實", "說", "雖", "為", "問", "無", "現", "樣", "應", "於", "與", "則", "這", "種"]
+	list2 = ["边", "变", "并", "从", "点", "东", "对", "发", "该", "个", "给", "关", "过", "还", "后", "欢", "会", "机", "几", "间", "见", "将", "进", "经", "觉", "开", "来", "里", "两", "吗", "么", "没", "们", "难", "让", "时", "实", "说", "虽", "为", "问", "无", "现", "样", "应", "于", "与", "则", "这", "种"]
+	#语料库来自 https://elearning.ling.sinica.edu.tw/cwordfreq.html
+	#从中选取前三百的繁体字部分，并在文章中随机检验，取存在率最高的前50个繁体字符
+	
+	tags = "" ; j = 0 ; list3 = []
+	for i in range(len(list1)):
+		char = list1[i]
+		if char in text:
+			j += 1
+			list3.append(char)
+			
+	s1 = set(list1)
+	s2 = set(list3)
+	s = s1.difference(s2)
+	chars = set2text(s)
+	k = len(list1) - j
+	
+	tags += "#txt #finished "
+	if j >= 0.2 * len(list1):
+		tags += "#zh_tw "
+		# print(k)  # 不存在的繁体字符数
+		# print(s)   #不存在的繁体字符
+	else:
+		tags += "#zh_cn "
+		if s2 !=set():
+			print(s2)  # 存在的繁体字符
+	return tags, chars
 
 
-def text2list(tags):  # 通过readlines，获得list对象
+def text2List(tags):  # 通过readlines，获得list对象
 	path = os.getcwd()
 	path = os.path.join(path, "tags.txt")
 	with open(path, "w", encoding="UTF8") as f:
@@ -544,7 +617,7 @@ def text2list(tags):  # 通过readlines，获得list对象
 	return taglist
 
 
-def translate(taglist):  # 获取英文标签
+def translateTags(taglist):  # 获取英文标签
 	tags2 = "" ; s = set()
 	for i in range(0, len(taglist)):
 		tag = taglist[i].replace("\n", "")
@@ -560,56 +633,66 @@ def translate(taglist):  # 获取英文标签
 			tag = tag.replace(" ", "")
 			tag = tag.replace("　", "")
 			tags2 += tag + " "
-			
-	tags1 = sorttags(s)  #对转换后的标签排序
+	tags1 = sortTags(s)  #对转换后的标签排序
 	return tags1, tags2
 	
 
-def textformat(textlist):
+def textFormat(textlist, newtags):
 	name = cc2.convert(textlist[0])
 	authro = "by #" + textlist[1].replace("作者：", "")
 	url = textlist[2].replace("网址：", "")
+	
 	tags = textlist[3].replace("标签：", "")
+	tags += newtags  #新增 #zh_tw 或 #zh_cn
 	tags = tags.replace(" ", "\n")
 	tags = cc1.convert(tags)  #转简体，只处理简体标签
-	list = text2list(tags)   #通过保存txt，将tag写入list
-	(tags1, tags2) = translate(list)  #获取已翻译/未翻译的标签
+	list = text2List(tags)   #通过保存txt，将tag写入list
+	(tags1, tags2) = translateTags(list)  #获取已翻译/未翻译的标签
+	
 	text = name + authro + tags1 + "\n特殊：" + tags2 + "\n" + url + "\n"
-	print(text)  #格式化输出
-	return tags2 #输出不支持的标签
+	print(text)   #格式化输出
+	return tags2  #输出不支持的标签
 
 
-def printtags(path):
-	pathlist = findfile(path)
-	dirstr = monthnow()  # 只处理本月的文件
-	s = set() ; j = 0
+def printTags(path):
+	pathlist = findFile(path)
+	dirstr = monthNow()  # 只处理本月的文件
+	s = set() ; j = 0 ; chars =""
 	for i in range(0, len(pathlist)):
 		path = pathlist[i]
 		(dir, name) = os.path.split(path)
+		(name, ext) = os.path.splitext(name)
 		if dirstr in dir:  # 只处理本月的文件
 			j += 1
-			(name, ext) = os.path.splitext(name)
 			if ext == ".docx":
-				textlist = opendocx4(path)
-			elif ext == ".text":
-				textlist = opentext4(path)
-			s.add(textformat(textlist))
+				textlist = openDocx4(path)
+				text     = openDocx (path)
+			elif ext == ".txt":
+				textlist = openText4(path)
+				text     = openText (path)
+				
+			if j >= 0:  #无用的if语句，保持上下几行缩进一致
+				newtags, char = addTags(text) #根据本文繁简添加标签
+				chars += char +"\n"*1
+				s.add(textFormat(textlist, newtags))
+	
+	saveTextDesktop("特殊标签.txt", s)
+	# saveTextDesktop("文字.txt", chars)
 	
 	if j != 0:
-		opennowdir()
+		openNowDir()
+		pass
 	else:
 		print("本月 " + dirstr + " 无新文档")
-	return s
 	
-
+	
 def main():
 	print("本月文档如下：")
 	print("\n"*2)
-	s = printtags(path)
-	savetags2text(s)
-	savedict2md()
-
-
+	printTags(path)
+	saveDict2md()
+	
+	
 if __name__ == "__main__":
 	path = os.path.join(os.getcwd())
 	path = path.replace("\工具", "")
